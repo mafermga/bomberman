5abfeec1222824c2a63f5987fbd48b28b8c5a176
mafermga <160082810+mafermga@users.noreply.github.com>
2024-04-19 02:00:26 +0000

    Version 2

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 
 
 bin/tui : src/tui_test.cpp	
-	g++ src/tui_test.cpp -o bin/tui -I include -lftxui-screen -lftxui-dom -lftxui-component 
+	g++ src/tui_test.cpp -o bin/tui -I include -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a 
 
 run : bin/tui
 	./bin/tui
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,30 +1,57 @@
 #include <ftxui/component/animation.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <experimental/random>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    const string texto = "Hola mundo"; 
-    Element textElement = text(texto);
-    Element dibujo = hbox(textElement | border);
 
-    Dimensions Alto = Dimension::Fixed(10);
-
-    Dimensions Ancho = Dimension ::Fixed(10);
-
-    Screen pantalla = Screen ::Create(Alto,Ancho);
-
-    Render(pantalla,dibujo);
-    pantalla.Print();
-    cout<<endl;
+    int fotograma = 0;
     
+    string reset;
+    while (true)
+    {
+        fotograma++;
 
+        int r = std::experimental::randint(0, 255);
+        int g = std::experimental::randint(0, 255);
+        int b = std::experimental::randint(0, 255); 
 
+        Element personaje = spinner(21, fotograma);
+        
+        //para color especifico 
+        Decorator colorFondo = bgcolor(Color::BlueLight);
+
+        //para colores aleatorios
+        //Decorator colorFondo = bgcolor (Color::RGB( r,g,b));
+        Decorator colorTexto = color(Color::RGB(b,g,r));
+
+        Element dibujo = border({hbox(personaje)  })| colorFondo | colorTexto; 
+
+        Dimensions Alto = Dimension::Fixed(10);
+
+        Dimensions Ancho = Dimension::Full();
+
+        Screen pantalla = Screen::Create(Ancho, Alto);
+
+        Render(pantalla, dibujo);
+
+        pantalla.Print();
+
+        reset = pantalla.ResetPosition();
+
+        cout << reset;
+
+        //para modificar el tiempo de como se mueve el monito
+        this_thread::sleep_for(0.8s);
+    }
 
     return 0;
 }
